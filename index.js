49a80c0af22183bbb534f61a1fbaf1324899bd12
Roman <rpol123453@gmail.com>
2022-08-18 16:21:31 +0300

    Add resolvers

--- index.js
+++ index.js
@@ -2,10 +2,37 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+const schema = require("./schema");
+const baseURL = "https://api.coincap.io/v2/";
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
-app.use("/graphql", graphqlHTTP({ graphiql: true }));
+
+const root = {
+  getAllCoins: ({ offset }) => {
+    return axios
+      .get(`https://api.coincap.io/v2/assets?offset=${offset}&limit=10`)
+      .then((res) => res.data.data);
+  },
+  getTop3Coins: () => {
+    return axios
+      .get(`https://api.coincap.io/v2/assets?limit=${3}`)
+      .then((res) => res.data.data);
+  },
+  getCurrencyPriceNow: ({ coinsToFetch }) => {
+    return axios
+      .get(`https://api.coincap.io/v2/assets?ids=${coinsToFetch}`)
+      .then((res) => res.data.data);
+  },
+  getCoinHistory: ({ id }) => {
+    return axios
+      .get(`https://api.coincap.io/v2/assets/${id}/history?interval=d1`)
+      .then((res) => res.data.data);
+  },
+};
+
+app.use("/graphql", graphqlHTTP({ graphiql: true, schema, rootValue: root }));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
