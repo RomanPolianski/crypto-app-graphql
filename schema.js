fd4798b25ca0bb1214e347c2068bb738db1c963f
Roman <rpol123453@gmail.com>
2022-08-18 16:21:07 +0300

    Create scheme

--- schema.js
+++ schema.js
@@ -1,3 +1,29 @@
+const { buildSchema } = require("graphql");
 
+const schema = buildSchema(`
+type Coin {
+    id: String,
+    rank: String,
+    symbol: String,
+    name: String,
+    supply: String,
+    maxSupply: String,
+    marketCapUsd: String,
+    volumeUsd24Hr: String,
+    priceUsd: String,
+    changePercent24Hr: String,
+    vwap24Hr: String,
+}
+type CoinHist {
+    priceUsd: String,
+    time: Float
+}
+type Query {
+    getAllCoins(offset: Int): [Coin],
+    getTop3Coins: [Coin],
+    getCurrencyPriceNow(coinsToFetch: String): [Coin],
+    getCoinHistory(id: String): [CoinHist]
+}
+`);
 
-
+module.exports = schema;
